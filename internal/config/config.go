abf8ac8e19ce8394cb210d2bc8be1535ba876b42
nvm <0x@deadbeef.rest>
2024-08-26 20:16:42 +0500

    add config

---
+++ internal/config/config.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"flag"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Driver     string `yaml:"driver"`
+	Datasource string `yaml:"datasource"`
+}
+
+func MustLoadConfig() *Config {
+	var path string
+
+	flag.StringVar(&path, "config", "./config.yaml", "path to config file")
+	flag.Parse()
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		panic("failed to find config file at: " + path)
+	}
+
+	var config Config
+	if err := cleanenv.ReadConfig(path, &config); err != nil {
+		panic("failed to read config file: " + err.Error())
+	}
+
+	return &config
+}
