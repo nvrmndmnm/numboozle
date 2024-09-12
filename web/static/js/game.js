e818117d1a3319d57d498277625b7136fd3df166
nvm <0x@deadbeef.rest>
2024-09-12 19:05:11 +0500

    add score requests to the game

--- web/static/js/game.js
+++ web/static/js/game.js
@@ -6,11 +6,20 @@ function updateTimer() {
     if (startTime) {
         const elapsedTime = (Date.now() - startTime) / 1000;
         document.getElementById("timer").innerText = `Time: ${elapsedTime.toFixed(1)} seconds`;
+        
+        return elapsedTime;
     }
+    
+    return 0;
 }
 
 document.addEventListener("htmx:configRequest", function (evt) {
     let clickedNumber = parseInt(evt.detail.parameters.number);
+
+    let target = evt.detail.target;
+    target.dataset.time = updateTimer() * 1000;
+
+    evt.detail.parameters.time = target.dataset.time;
 
     if (clickedNumber !== currentNumber) {
         evt.preventDefault();
@@ -33,7 +42,8 @@ document.addEventListener("htmx:afterRequest", function (evt) {
             clearInterval(timerInterval);
             let endTime = Date.now();
             let totalTime = (endTime - startTime) / 1000;
-            document.getElementById("score").innerText = `your time: ${totalTime.toFixed(2)} seconds`;
+
+            document.getElementById("timer").innerText = `Time: ${totalTime.toFixed(2)} seconds`;
 
             startTime = null;
             currentNumber = 1;
@@ -41,11 +51,6 @@ document.addEventListener("htmx:afterRequest", function (evt) {
             htmx.trigger("#game-area", "reload");
         } else {
             currentNumber++;
-
-            if (mode === 2) {
-                target.innerHTML = "";
-                document.querySelector(`[data-num="${currentNumber}"]`).innerHTML = currentNumber;
-            }
         }
     }
 });
